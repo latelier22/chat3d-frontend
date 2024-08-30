717df7382d5bf7937329be17b45e6636557e75f7
latelier22 <latelier.cyrille@gmail.com>
2024-08-30 10:11:26 +0200

    init

---
+++ src/hooks/useChat.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const port = import.meta.env.VITE_BACKEND_PORT;
+const backendUrl = `http://localhost:${port}`;
+
+console.log(backendUrl); // Ceci affichera "http://localhost:3210"
+
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const chat = async (message) => {
+    setLoading(true);
+    const data = await fetch(`${backendUrl}/chat`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+    const resp = (await data.json()).messages;
+    setMessages((messages) => [...messages, ...resp]);
+    setLoading(false);
+  };
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState();
+  const [loading, setLoading] = useState(false);
+  const [cameraZoomed, setCameraZoomed] = useState(true);
+  const onMessagePlayed = () => {
+    setMessages((messages) => messages.slice(1));
+  };
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setMessage(messages[0]);
+    } else {
+      setMessage(null);
+    }
+  }, [messages]);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        chat,
+        message,
+        onMessagePlayed,
+        loading,
+        cameraZoomed,
+        setCameraZoomed,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => {
+  const context = useContext(ChatContext);
+  if (!context) {
+    throw new Error("useChat must be used within a ChatProvider");
+  }
+  return context;
+};
