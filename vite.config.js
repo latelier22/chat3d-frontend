24f9c2e514e49dacdddbdf30f3793f3b18944bc0
latelier22 <latelier.cyrille@gmail.com>
2024-08-31 12:32:01 +0200

    vite config pour use client

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
+
